04e70136bc188a2ef186ecbfd233119b984f00f6
Akritidis Theodoros <38220593+ThodorisAkr@users.noreply.github.com>
2020-04-04 21:34:14 +0300

    Add files via upload

---
+++ MyMapper.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[77]:
+
+
+import json
+
+with open('dblps.json', 'r') as infile:
+    data = infile.read()
+    new_data = data.replace('}\n{', '},{')
+    json_data = json.loads(f'[{new_data}]')
+    
+    for data in json_data:
+        for name in data['authors']:
+            print (name,data['authors'].index(name)+1 , 1)
+            
+
+#cat ~/input | ./mapper.py | \sort | ./reducer.py
+    
+    
+
+    
+    
+
+
+# In[ ]:
+
+
+
+
