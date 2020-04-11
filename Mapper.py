d81b1b7e766dcd5c4f13c460d7238747462e91e0
Akritidis Theodoros <38220593+ThodorisAkr@users.noreply.github.com>
2020-04-11 21:31:03 +0300

    Add files via upload

---
+++ Mapper.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#!/usr/bin/env python3
+
+import sys
+import json
+
+# input comes from STDIN (standard input)
+for line in open('dblp-ref-0.json'):
+    newLine = json.loads(line)
+    
+    for key,value in newLine.items():
+        if(key =='authors'):
+            for name in value:
+                print(name,value.index(name)+1 , 1)
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
